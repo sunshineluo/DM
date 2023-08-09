950662d98e0c232afe8945b4c5afa0f447a48640
Cloudflare233 <Cloudflare233@yandex.com>
2023-08-09 17:11:21 +0800

    fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,8 @@ import Link from "next/link"
 export default function Navbar(){
     return(
         <div className="">
-            {nav.map((item)=>(
-                <Link href={item.href}>
+            {nav.map((item, index)=>(
+                <Link key={index} href={item.href}>
                     <button className="font-medium text-lg">
                         {item.title}
                     </button>
