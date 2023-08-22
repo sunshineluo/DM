17e73b26a170b0acc27d8380bcfa88c396547254
Cloudflare233 <Cloudflare233@yandex.com>
2023-08-22 13:50:19 +0800

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="zh">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
